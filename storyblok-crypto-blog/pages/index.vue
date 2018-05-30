49479bb567c18a22933b075c26d0355f2d0130d1
gitchili <jameslphillips.media@gmail.com>
2018-05-30 06:55:39 -0600

    post component & some styleing

--- storyblok-crypto-blog/pages/index.vue
+++ storyblok-crypto-blog/pages/index.vue
@@ -1,40 +1,37 @@
 <template>
-  <section class="container">
-    <nuxt-link
+  <section id="posts">
+    <PostPreview
       v-for="post in posts"
-    :key="post.id"
-    :to="post.id">
-    <article class="post-preview">
-      <div
-     
-      class="post-preview-thumbnail"></div>
-      <div class="post-review-content"></div>
-      <h1>{{ post.title }}</h1>
-      <p>{{ post.previewText }}</p>
-    </article>
-    </nuxt-link>
- 
-    
+      :key="post.id"
+      :title="post.title"
+      :excerpt="post.previewText"
+      :thumbnailImage="post.thumbnailUrl"
+      :id="post.id" />
   </section>
 </template>
 
 <script>
-
+import PostPreview from "@/components/Blog/PostPreview";
 export default {
+  components: {
+    PostPreview
+  },
   data() {
     return {
       posts: [
         {
-          title: "BlockChain News",
-          previewText: 'Find the latest BlockChain.news',
-          thumbnailUrl: "",
-          id: "a-new-Beginning"
+          title: "A New Beginning",
+          previewText: "This will be awesome, don't miss it!",
+          thumbnailUrl:
+            "https://cdn-images-1.medium.com/max/2000/1*knBQko9CR0e4xRfnSp-Hyg.jpeg",
+          id: "a-new-beginning"
         },
         {
-          title: "Crypto Tracker",
-          previewText: 'Track crypto-assets',
-          thumbnailUrl: "",
-          id: "a-second-Beginning"
+          title: "A Second Beginning",
+          previewText: "This will be awesome, don't miss it!",
+          thumbnailUrl:
+            "https://hacked.com/wp-content/uploads/2018/03/cryptocurrency.jpg",
+          id: "a-second-beginning"
         }
       ]
     };
@@ -42,6 +39,18 @@ export default {
 };
 </script>
 
-<style>
-
+<style scoped>
+#posts {
+  padding-top: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+@media (min-width: 45rem) {
+  #posts {
+    flex-direction: row;
+  }
+}
 </style>
+
