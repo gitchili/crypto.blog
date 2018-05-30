7758a92c7790a134bc945f88607946c08d9dcb4e
gitchili <jameslphillips.media@gmail.com>
2018-05-30 05:26:23 -0600

    post component build-test

--- storyblok-crypto-blog/pages/index.vue
+++ storyblok-crypto-blog/pages/index.vue
@@ -1,5 +1,19 @@
 <template>
   <section class="container">
+    <nuxt-link
+      v-for="post in posts"
+    :key="post.id"
+    :to="post.id">
+    <article class="post-preview">
+      <div
+     
+      class="post-preview-thumbnail"></div>
+      <div class="post-review-content"></div>
+      <h1>{{ post.title }}</h1>
+      <p>{{ post.previewText }}</p>
+    </article>
+    </nuxt-link>
+ 
     
   </section>
 </template>
@@ -7,8 +21,25 @@
 <script>
 
 export default {
-
-}
+  data() {
+    return {
+      posts: [
+        {
+          title: "BlockChain News",
+          previewText: 'Find the latest BlockChain.news',
+          thumbnailUrl: "",
+          id: "a-new-Beginning"
+        },
+        {
+          title: "Crypto Tracker",
+          previewText: 'Track crypto-assets',
+          thumbnailUrl: "",
+          id: "a-second-Beginning"
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style>
